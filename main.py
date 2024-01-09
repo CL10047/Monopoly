da12e273611f72b4902115d27217c4e7956f08b7
Chris Lee <christopher.lee1004@gmail.com>
2024-01-09 18:39:17 -0500

    initial commit v2

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from datetime import timedelta
+import time
+from sqlalchemy import exc, func, desc, asc
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Monopoly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Monopoly.db'
+db = SQLAlchemy(app)
+
+app.run(host='0.0.0.0', port=99, debug=True)
